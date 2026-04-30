fef1ee2447866dc7a510f657c1d73e07a343f09b
Safaa <Safaaalnhal7@gmail.com>
2026-05-01 02:52:30 +0300

    fix: handle unused error in useTask

--- task-frontend/eslint.config.js
+++ task-frontend/eslint.config.js
@@ -21,9 +21,12 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
-
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn"
-    }
+  "@typescript-eslint/no-explicit-any": "warn",
+  "@typescript-eslint/no-unused-vars": "warn",
+  "@typescript-eslint/no-unsafe-assignment": "warn",
+  "@typescript-eslint/no-unsafe-member-access": "warn",
+  "@typescript-eslint/no-unsafe-call": "warn"
+}
   },
 ])

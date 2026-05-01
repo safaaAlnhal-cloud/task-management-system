78f129d2acbdcd6a1a0dc19f97c2bc54acdea9e8
Safaa <Safaaalnhal7@gmail.com>
2026-05-01 04:47:54 +0300

    fix: CI eslint + test script

--- task-frontend/eslint.config.js
+++ task-frontend/eslint.config.js
@@ -1,7 +1,14 @@
-export default defineConfig([
-  globalIgnores(['dist']),
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import { globalIgnores } from "eslint/config";
+
+export default [
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -11,13 +18,11 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
-
-    
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-call": "off",
-    }
+    },
   },
-])
+];
